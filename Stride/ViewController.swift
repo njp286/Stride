39413af70075a4078eed30e424325096e47c3891
Nate PiSierra <njp286@nyu.edu>
2016-04-06 19:10:13 -0400

    Initial Commit

---
+++ Stride/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stride
+//
+//  Created by Nathaniel PiSierra on 4/6/16.
+//  Copyright © 2016 Nathaniel PiSierra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ StrideTests/StrideTests.swift
@@ -0,0 +1,36 @@
+//
+//  StrideTests.swift
+//  StrideTests
+//
+//  Created by Nathaniel PiSierra on 4/6/16.
+//  Copyright © 2016 Nathaniel PiSierra. All rights reserved.
+//
+
+import XCTest
+@testable import Stride
+
+class StrideTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
